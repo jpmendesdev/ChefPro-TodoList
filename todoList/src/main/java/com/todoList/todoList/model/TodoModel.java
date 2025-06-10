06ee297f156dca6833d4cf4da7c3c56a75e5b3a3
jpmendesdev <jotamendes944@gmail.com>
2025-06-10 19:36:05 -0300

    Criação dos atributos da classe model, utilizando encapsulamento e as notações devidas do Spring, Entity para identificar a classe como entidade e Table para passarmos os atributos para a table no banco de dados. No atributo id foi usado a notação Id para identifcar devidademente e GeneratedValue para incrementar o valor do id automaticamente

--- todoList/src/main/java/com/todoList/todoList/model/TodoModel.java
+++ todoList/src/main/java/com/todoList/todoList/model/TodoModel.java
@@ -1,5 +1,62 @@
 package com.todoList.todoList.model;
 
-public class TodoModel {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "TodoList")
+public class TodoModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String titulo;
+    private String restricao;
+    private String status;
+
+    public TodoModel(long id, String titulo, String restricao, String status) {
+        this.id = id;
+        this.titulo = titulo;
+        this.restricao = restricao;
+        this.status = status;
+    }
+
+    public TodoModel(){}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getRestricao() {
+        return restricao;
+    }
+
+    public void setRestricao(String restricao) {
+        this.restricao = restricao;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    
+    
 }
