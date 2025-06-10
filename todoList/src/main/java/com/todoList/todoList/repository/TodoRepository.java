7be8918a56be3082f163456ac7087850f43b26f3
jpmendesdev <jotamendes944@gmail.com>
2025-06-10 19:43:58 -0300

    Criação da interface TodoRepository no pacote repository, que será utilizada para a injeção de dependências na classe service

--- todoList/src/main/java/com/todoList/todoList/repository/TodoRepository.java
+++ todoList/src/main/java/com/todoList/todoList/repository/TodoRepository.java
@@ -1,5 +1,10 @@
 package com.todoList.todoList.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public class TodoRepository {
+import com.todoList.todoList.model.TodoModel;
+
+
+
+public interface TodoRepository extends JpaRepository<TodoModel, Long> {
 
 }
