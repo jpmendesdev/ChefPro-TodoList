2051bd199be824c9b8a4cc00c12bd6a60262d413
jpmendesdev <jotamendes944@gmail.com>
2025-06-12 15:30:26 -0300

    Implementação da classe controller, definindo a rota /todos onde poderão ser feitas as chamadas http, como também aplicando a injeção de dependências com TodoService sendo chamado na classe e implementado via construtor

--- todoList/src/main/java/com/todoList/todoList/controller/TodoController.java
+++ todoList/src/main/java/com/todoList/todoList/controller/TodoController.java
@@ -1,5 +1,22 @@
 package com.todoList.todoList.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.todoList.todoList.service.TodoService;
+
+@RestController
+@RequestMapping("/todos")
 public class TodoController {
 
+    private TodoService todoService;
+
+    public TodoController(TodoService todoService){
+        this.todoService = todoService;
+    }
+
+    public TodoController(){}
+
+    
+    
 }
