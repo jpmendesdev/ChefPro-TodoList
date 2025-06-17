7430eadb0c1e71cc16b567dce832ba1af7d47b9f
jpmendesdev <jotamendes944@gmail.com>
2025-06-16 22:47:41 -0300

    Criação de constantes para cada elemento do formulário titulo, descricao ...

---
+++ todoList/src/main/java/com/todoList/todoList/view/front/js/main.js
@@ -0,0 +1,14 @@
+const formulario = document.querySelector("#formulario");
+const titulo = document.querySelector(".titulo");
+const descricao = document.querySelector(".descricao");
+const status = document.querySelector(".status");
+const prioridade = document.querySelector(".prioridade");
+const btnCadastrar = document.querySelector("#formulario button[type= 'submit']");
+
+let modoEdicao = false;
+let idTarefaEditando = null;
+
+function salvarTarefa(event){
+    
+}
+
